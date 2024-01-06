31609205b5ecde54c20c33f981b3be7210769993
TomasLukes <lukest@seznam.cz>
2024-01-06 17:47:44 +0100

    add: basic project setup

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,16 +1,17 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="w-screen bg-black text-white">
-      <form className='mx-auto flex flex-col gap-4 w-64 p-4 border border-yellow-300 rounded-md'>
-        <fieldset className='text-black flex flex-col gap-2'>
-
-        <input type="email" />
-        <input type='password' />
+      <form className="mx-auto flex flex-col gap-4 w-64 p-4 border border-yellow-300 rounded-md">
+        <fieldset className="text-black flex flex-col gap-2">
+          <input type="email" />
+          <input type="password" />
         </fieldset>
-        <button type='submit' className='border-2 border-yellow-500 rounded-lg p-2'>Přihlásit se</button>
+        <button type="submit" className="border-2 border-yellow-500 rounded-lg p-2">
+          Přihlásit se
+        </button>
       </form>
     </main>
-  )
+  );
 }
