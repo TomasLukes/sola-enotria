fbb9a4f15a843a6984314900586375c270a38d43
TomasLukes <lukest@seznam.cz>
2024-01-23 19:32:17 +0100

    add: CardsCarousel, images

--- app/components/AboutUs/AboutUs.tsx
+++ app/components/AboutUs/AboutUs.tsx
@@ -1,25 +1,113 @@
 import clsx from 'clsx';
 import Image from 'next/image';
+import {
+  CardsCarousel,
+  CardsCarouselContent,
+  CardsCarouselItem,
+} from '../CardsCarousel/CardsCarousel';
 
 const AboutUs = ({ className }: IClassName) => {
   return (
-    <section
-      className={clsx(
-        'container-base h-[100dvh] py-16 md:py-32 lg:p  y-40 flex justify-start flex-col md:flex-row gap-5 md:gap-8 bg-grain bg-contain',
-        className
-      )}
-    >
-      <div className="w-full md:w-1/2">
-        <h3 className="text-3xl underline decoration-accent pb-4">O nás</h3>
-        <p>
-          Také máte rádi přátelskou obsluhu, vše bez komplikací a výborné jídlo? Tak to jste u nás
-          správně! Náš tým z Pizza Bohnice připraví jídlo pro každou příležitost, ale nabízí také
-          širokou škálu chutných pokrmů a osvěžujících nápojů. Čerstvost surovin je základ naší
-          rozmanité italské kuchyně.
-        </p>
-      </div>
-      <div className="relative w-full md:w-1/2 h-[260px] md:h-[360px] rounded-md shadow-lg overflow-hidden">
-        <Image fill src="/assets/restaurant/interier.jpg" alt="Sola Enotria interier" />
+    <section className={clsx('bg-grain bg-cover', className)}>
+      <div>
+        <div className="h-[100dvh] py-16 md:py-32 lg:py-40 flex justify-start flex-col md:flex-row gap-5 md:gap-8">
+          <div className="container-full w-full md:w-1/2">
+            <h3 className="text-3xl underline decoration-accent pb-4">O nás</h3>
+            <p>
+              Také máte rádi přátelskou obsluhu, vše bez komplikací a výborné jídlo? Tak to jste u
+              nás správně! Náš tým z Pizza Bohnice připraví jídlo pro každou příležitost, ale nabízí
+              také širokou škálu chutných pokrmů a osvěžujících nápojů. Čerstvost surovin je základ
+              naší rozmanité italské kuchyně.
+            </p>
+          </div>
+          <div className="relative w-full md:w-1/2 h-[260px] md:h-[360px] rounded-md shadow-lg overflow-hidden hidden lg:block">
+            <Image fill src="/assets/restaurant/interier-01.jpg" alt="Sola Enotria interier" />
+          </div>
+          <CardsCarousel className="w-full lg:hidden">
+            <CardsCarouselContent>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-01.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-02.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-03.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-04.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-05.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-06.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-07.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+              <CardsCarouselItem>
+                <div className="relative w-[250px] h-[250px] rounded-md shadow-lg overflow-hidden">
+                  <Image
+                    fill
+                    src="/assets/restaurant/interier-08.jpg"
+                    alt="Sola Enotria interier"
+                    objectFit="cover"
+                  />
+                </div>
+              </CardsCarouselItem>
+            </CardsCarouselContent>
+          </CardsCarousel>
+        </div>
       </div>
     </section>
   );
---
+++ app/components/CardsCarousel/CardsCarousel.tsx
@@ -0,0 +1,174 @@
+'use client';
+
+import * as React from 'react';
+import clsx from 'clsx';
+import useEmblaCardsCarousel, {
+  type EmblaCarouselType as CardsCarouselApi,
+  type EmblaOptionsType as CardsCarouselOptions,
+  type EmblaPluginType as CardsCarouselPlugin,
+} from 'embla-carousel-react';
+
+type CardsCarouselProps = {
+  opts?: CardsCarouselOptions;
+  plugins?: CardsCarouselPlugin[];
+  orientation?: 'horizontal' | 'vertical';
+  setApi?: (api: CardsCarouselApi) => void;
+};
+
+type CardsCarouselContextProps = {
+  CardsCarouselRef: ReturnType<typeof useEmblaCardsCarousel>[0];
+  api: ReturnType<typeof useEmblaCardsCarousel>[1];
+  scrollPrev: () => void;
+  scrollNext: () => void;
+  canScrollPrev: boolean;
+  canScrollNext: boolean;
+} & CardsCarouselProps;
+
+const CardsCarouselContext = React.createContext<CardsCarouselContextProps | null>(null);
+
+const useCardsCarousel = (): CardsCarouselContextProps => {
+  const context = React.useContext(CardsCarouselContext);
+
+  if (!context) {
+    throw new Error('useCardsCarousel must be used within a <CardsCarousel />');
+  }
+
+  return context;
+};
+
+const CardsCarousel = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & CardsCarouselProps
+>(({ orientation = 'horizontal', opts, setApi, plugins, className, children, ...props }, ref) => {
+  const [CardsCarouselRef, api] = useEmblaCardsCarousel(
+    {
+      ...opts,
+      axis: orientation === 'horizontal' ? 'x' : 'y',
+    },
+    plugins
+  );
+  const [canScrollPrev, setCanScrollPrev] = React.useState(false);
+  const [canScrollNext, setCanScrollNext] = React.useState(false);
+
+  const onSelect = React.useCallback((api: CardsCarouselApi) => {
+    if (!api) {
+      return;
+    }
+
+    setCanScrollPrev(api.canScrollPrev());
+    setCanScrollNext(api.canScrollNext());
+  }, []);
+
+  const scrollPrev = React.useCallback(() => {
+    api?.scrollPrev();
+  }, [api]);
+
+  const scrollNext = React.useCallback(() => {
+    api?.scrollNext();
+  }, [api]);
+
+  const handleKeyDown = React.useCallback(
+    (event: React.KeyboardEvent<HTMLDivElement>) => {
+      if (event.key === 'ArrowLeft') {
+        event.preventDefault();
+        scrollPrev();
+      } else if (event.key === 'ArrowRight') {
+        event.preventDefault();
+        scrollNext();
+      }
+    },
+    [scrollPrev, scrollNext]
+  );
+
+  React.useEffect(() => {
+    if (!api || !setApi) {
+      return;
+    }
+
+    setApi(api);
+  }, [api, setApi]);
+
+  React.useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    onSelect(api);
+    api.on('reInit', onSelect);
+    api.on('select', onSelect);
+
+    return (): void => {
+      api?.off('select', onSelect);
+    };
+  }, [api, onSelect]);
+
+  return (
+    <CardsCarouselContext.Provider
+      value={{
+        CardsCarouselRef,
+        api: api,
+        opts,
+        orientation: orientation || (opts?.axis === 'y' ? 'vertical' : 'horizontal'),
+        scrollPrev,
+        scrollNext,
+        canScrollPrev,
+        canScrollNext,
+      }}
+    >
+      <div
+        ref={ref}
+        onKeyDownCapture={handleKeyDown}
+        className={clsx('relative', className)}
+        role="region"
+        aria-roledescription="CardsCarousel"
+        {...props}
+      >
+        {children}
+      </div>
+    </CardsCarouselContext.Provider>
+  );
+});
+CardsCarousel.displayName = 'CardsCarousel';
+
+const CardsCarouselContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    const { CardsCarouselRef, orientation } = useCardsCarousel();
+
+    return (
+      <div
+        ref={CardsCarouselRef}
+        className="overflow-x-hidden overflow-y-visible lg:overflow-visible"
+      >
+        <div
+          ref={ref}
+          className={clsx('flex', orientation === 'horizontal' ? '' : 'flex-col', className)}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+CardsCarouselContent.displayName = 'CardsCarouselContent';
+
+const CardsCarouselItem = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => {
+    const { orientation } = useCardsCarousel();
+
+    return (
+      <div
+        ref={ref}
+        role="group"
+        aria-roledescription="slide"
+        className={clsx(
+          '',
+          orientation === 'horizontal' ? 'first:pl-4 first:lg:pl-0 pr-4 lg:pr-10' : 'pt-4',
+          className
+        )}
+        {...props}
+      />
+    );
+  }
+);
+CardsCarouselItem.displayName = 'CardsCarouselItem';
+
+export { CardsCarousel, type CardsCarouselApi, CardsCarouselContent, CardsCarouselItem };
