3e594d2b3ae9c2ce7c6b6e0062d9793c20987cc6
TomasLukes <lukest@seznam.cz>
2024-01-28 16:31:06 +0100

    fix: minor text styling

--- app/components/AboutUs/AboutUs.tsx
+++ app/components/AboutUs/AboutUs.tsx
@@ -14,13 +14,15 @@ const AboutUs = ({ className }: IClassName) => {
     <section className={clsx('bg-grain bg-cover', className)}>
       <div>
         <div className="h-[100dvh] py-16 md:py-32 lg:py-40 flex justify-start flex-col md:flex-row gap-5 md:gap-8">
-          <div className="container-full w-full md:w-1/2">
-            <h3 className="text-3xl underline decoration-accent pb-4">O nás</h3>
-            <p>
+          <div className="container-base w-full md:w-1/2">
+            <h3 className="text-4xl underline decoration-accent pb-4">O nás</h3>
+            <p className="paragraph pb-2">
               Také máte rádi přátelskou obsluhu, vše bez komplikací a výborné jídlo? Tak to jste u
-              nás správně! Náš tým z Pizza Bohnice připraví jídlo pro každou příležitost, ale nabízí
-              také širokou škálu chutných pokrmů a osvěžujících nápojů. Čerstvost surovin je základ
-              naší rozmanité italské kuchyně.
+              nás správně! Náš tým z Pizza Bohnice připraví jídlo pro každou příležitost.
+            </p>
+            <p className="paragraph">
+              Nabízí také širokou škálu chutných pokrmů a osvěžujících nápojů. Čerstvost surovin je
+              základ naší rozmanité italské kuchyně.
             </p>
           </div>
           <div className="relative w-full md:w-1/2 h-[260px] md:h-[360px] rounded-md shadow-lg overflow-hidden hidden lg:block">
