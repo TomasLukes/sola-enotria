70f96b8839cd432039bb084b6ed4075661eb93d4
TomasLukes <lukest@seznam.cz>
2024-01-30 20:01:18 +0100

    fix: About Us section for desktop

--- app/components/AboutUs/AboutUs.tsx
+++ app/components/AboutUs/AboutUs.tsx
@@ -11,24 +11,23 @@ const AboutUs = ({ className }: IClassName) => {
   const numberOfImages = Array.from({ length: 8 });
 
   return (
-    <section className={clsx('bg-grain bg-cover', className)}>
+    <section className={clsx('bg-grain bg-cover overflow-hidden', className)}>
       <div>
-        <div className="h-[100dvh] py-16 md:py-32 lg:py-40 flex justify-start flex-col md:flex-row gap-5 md:gap-8">
-          <div className="container-base w-full md:w-1/2">
-            <h3 className="text-4xl underline decoration-accent pb-4">O nás</h3>
-            <p className="paragraph pb-2">
-              Také máte rádi přátelskou obsluhu, vše bez komplikací a výborné jídlo? Tak to jste u
-              nás správně! Náš tým z Pizza Bohnice připraví jídlo pro každou příležitost.
-            </p>
-            <p className="paragraph">
-              Nabízí také širokou škálu chutných pokrmů a osvěžujících nápojů. Čerstvost surovin je
-              základ naší rozmanité italské kuchyně.
-            </p>
+        <div className="h-[100dvh] py-16 md:py-32 lg:py-40 flex justify-start flex-col gap-5 md:gap-8">
+          <div className="container-base flex flex-col md:flex-row">
+            <h3 className="w-full md:w-1/3 heading underline decoration-accent pb-4">O nás</h3>
+            <div className="w-full md:w-2/3">
+              <p className="paragraph pb-2">
+                Také máte rádi přátelskou obsluhu, vše bez komplikací a výborné jídlo? Tak to jste u
+                nás správně! Náš tým z Pizza Bohnice připraví jídlo pro každou příležitost.
+              </p>
+              <p className="paragraph">
+                Nabízí také širokou škálu chutných pokrmů a osvěžujících nápojů. Čerstvost surovin
+                je základ naší rozmanité italské kuchyně.
+              </p>
+            </div>
           </div>
-          <div className="relative w-full md:w-1/2 h-[260px] md:h-[360px] rounded-md shadow-lg overflow-hidden hidden lg:block">
-            <Image fill src="/assets/restaurant/interier-01.jpg" alt="Sola Enotria interier" />
-          </div>
-          <CardsCarousel className="w-full lg:hidden">
+          <CardsCarousel className="lg:container-base w-full mt-8 md:mt-12">
             <CardsCarouselContent>
               {numberOfImages.map((_, index) => {
                 return (
