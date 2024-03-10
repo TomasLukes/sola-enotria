a647d06e8e04abb2faf416445eb779795dae9514
TomasLukes <lukest@seznam.cz>
2024-03-10 18:52:15 +0100

    fix: spacing, layout breaks

--- app/components/Contacts/Contacts.tsx
+++ app/components/Contacts/Contacts.tsx
@@ -6,19 +6,21 @@ const Contacts = (): ReactElement => {
     <section className="w-full gradient-dark text-white">
       <div className="container-base py-8">
         <div>
-          <h5 className="subheading pb-0.5 underline decoration-accent">Kontakty</h5>
-          <span className="flex items-center gap-2">
-            <HomeIcon className="w-4 h-4" />
-            <p className="paragraph">Adresa: Lodžská 850/6, 181 00 Praha 8</p>
-          </span>
-          <span className="flex items-center gap-2">
-            <PhoneArrowUpRightIcon className="w-3.5 h-3.5" />
-            <p className="paragraph">Telefon: +420 773 510 277</p>
-          </span>
-          <span className="flex items-center gap-2">
-            <EnvelopeIcon className="w-4 h-4" />
-            <p className="paragraph">Email: info@solaenotria.cz</p>
-          </span>
+          <h5 className="subheading pb-2 underline decoration-accent">Kontakty</h5>
+          <ul className="flex flex-col gap-1">
+            <li className="flex items-center gap-2">
+              <HomeIcon className="w-4 h-4" />
+              <p className="paragraph">Adresa: Lodžská 850/6, 181 00 Praha 8</p>
+            </li>
+            <li className="flex items-center gap-2">
+              <PhoneArrowUpRightIcon className="w-3.5 h-3.5" />
+              <p className="paragraph">Telefon: +420 773 510 277</p>
+            </li>
+            <li className="flex items-center gap-2">
+              <EnvelopeIcon className="w-4 h-4" />
+              <p className="paragraph">Email: info@solaenotria.cz</p>
+            </li>
+          </ul>
         </div>
       </div>
     </section>
--- app/components/DailyMenu/MenuItem.tsx
+++ app/components/DailyMenu/MenuItem.tsx
@@ -1,11 +1,11 @@
 const MenuItem = ({ name, description, price }: any) => {
   return (
-    <div className="w-full flex items-start justify-between gap-2 md:gap-4 py-4 text-white">
+    <div className="w-full flex items-start justify-between gap-4 py-4 text-white">
       <span>
         <p className="text-base font-medium">{name}</p>
         {Boolean(description) && <p className="text-sm font-light">{description}</p>}
       </span>
-      <p className="italic text-nowrap shrink-0">{price},- Kč</p>
+      <p className="min-w-fit italic text-nowrap shrink-0">{price},- Kč</p>
     </div>
   );
 };
