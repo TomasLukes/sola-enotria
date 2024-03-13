02a0dc125d0dc8145b2d4b597c5e8c93abf8186d
TomasLukes <lukest@seznam.cz>
2024-03-13 13:29:30 +0100

    fix: close menu when redirected to section

--- app/components/Navigation/NavigationMobile.tsx
+++ app/components/Navigation/NavigationMobile.tsx
@@ -5,9 +5,9 @@ import { useState } from 'react';
 export default function NavigationMobile() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  function handleClick() {
-    setMobileMenuOpen(!mobileMenuOpen);
-  }
+  const toggleMobileMenu = (): void => {
+    setMobileMenuOpen((prev) => !prev);
+  };
 
   return (
     <nav className="text-xl font-medium lg:hidden">
@@ -17,7 +17,7 @@ export default function NavigationMobile() {
           aria-label="Mobile hamburger icon"
           type="button"
           className="flex items-center p-3 md:p-6"
-          onClick={handleClick}
+          onClick={() => toggleMobileMenu()}
         >
           {/* Hamburger icon */}
           <div
@@ -34,28 +34,66 @@ export default function NavigationMobile() {
 
       {/* Mobile + tablet menu */}
       <div className="lg:hidden">
-        <div
+        <ul
           id="menu"
           className={`${
             !mobileMenuOpen && 'hidden'
           } absolute z-50 flex flex-col gap-4 items-start p-8 self-end py-8 rounded-lg paragraph font-medium
          bg-neutral-200 text-neutral-900 sm:w-auto sm:self-center left-6 right-6 drop-shadow-md mx-auto md:mx-8`}
         >
-          <a href="#o-nas">O nás</a>
-          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm"></span>
-          <a href="#poledni-menu">Polední menu</a>
-          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm"></span>
-          <a href="/jidelni-listek">Jídelní lístek</a>
-          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm"></span>
-          <a href="#kudy-k-nam">Kudy k nám?</a>
-          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm"></span>
-          <a className="flex items-center gap-2 font-normal paragraph" href="tel:+420123456789">
-            <span className="rounded-full bg-accent p-2">
-              <PhoneArrowUpRightIcon className="w-4 h-4 stroke-white animate-pulse" />
-            </span>
-            +420 123 456 789
-          </a>
-        </div>
+          <li>
+            <a
+              href="#o-nas"
+              onClick={() => {
+                toggleMobileMenu();
+              }}
+            >
+              O nás
+            </a>
+          </li>
+          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm" />
+          <li>
+            <a
+              href="#poledni-menu"
+              onClick={() => {
+                toggleMobileMenu();
+              }}
+            >
+              Polední menu
+            </a>
+          </li>
+          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm" />
+          <li>
+            <a
+              href="/jidelni-listek"
+              onClick={() => {
+                toggleMobileMenu();
+              }}
+            >
+              Jídelní lístek
+            </a>
+          </li>
+          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm" />
+          <li>
+            <a
+              href="#kudy-k-nam"
+              onClick={() => {
+                toggleMobileMenu();
+              }}
+            >
+              Kudy k nám?
+            </a>
+          </li>
+          <span className="w-full border border-solid border-b-1 border-gray-300 drop-shadow-sm" />
+          <li>
+            <a className="flex items-center gap-2 font-normal paragraph" href="tel:+420123456789">
+              <span className="rounded-full bg-accent p-2">
+                <PhoneArrowUpRightIcon className="w-4 h-4 stroke-white animate-pulse" />
+              </span>
+              <p>+420 123 456 789</p>
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
