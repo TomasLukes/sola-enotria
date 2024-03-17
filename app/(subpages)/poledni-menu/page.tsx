84767bd52b097849c0aa29f158df244c6a6831da
TomasLukes <lukest@seznam.cz>
2024-03-17 20:17:02 +0100

    add: "Poledni menu" page mock up

---
+++ app/(subpages)/poledni-menu/page.tsx
@@ -0,0 +1,15 @@
+import Logo from '@/app/components/UI/Logo';
+
+const PoledniMenu = () => {
+  return (
+    <main className="h-screen w-screen gradient-dark text-white grid place-content-center">
+      <Logo className="w-[250px] h-full" />
+      <div className="mt-12">
+        <h1 className="heading text-center">Polední menu</h1>
+        <p className="mt-4">... bude doplněno 🍕</p>
+      </div>
+    </main>
+  );
+};
+
+export default PoledniMenu;
