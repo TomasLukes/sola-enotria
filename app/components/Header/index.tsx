98a771ceb981d966a516441f735f39b3d06e5d82
TomasLukes <lukest@seznam.cz>
2024-03-17 13:46:25 +0100

    fix: replace PNG with SVG logo

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -2,20 +2,13 @@ import Image from 'next/image';
 import Navigation from '../Navigation';
 import { ReactElement } from 'react';
 import clsx from 'clsx';
+import Logo from '../UI/Logo';
 
 const Header = ({ className }: IClassName): ReactElement => {
   return (
     <header className={clsx('w-full fixed z-50 gradient-dark text-light-primary', className)}>
       <div className="container-base h-16 md:h-20 flex justify-between items-center">
-        <div className="relative h-[80px] w-[120px] md:h-[160px] md:w-[240px] flex-shrink-0">
-          <Image
-            fill
-            priority
-            src={`/assets/logo.png`}
-            alt="Sola Enotria logo"
-            className="object-contain"
-          />
-        </div>
+        <Logo className="h-full w-[210px] flex-shrink-0" />
         <Navigation />
       </div>
     </header>
