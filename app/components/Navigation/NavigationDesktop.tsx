4e6254f0b792b1d55c6b4ee9cf2aba4b9b264b10
TomasLukes <lukest@seznam.cz>
2024-03-17 19:43:56 +0100

    add: hover states

--- app/components/Navigation/NavigationDesktop.tsx
+++ app/components/Navigation/NavigationDesktop.tsx
@@ -6,22 +6,34 @@ const NavigationDesktop = (): ReactElement => {
     <nav className="hidden lg:block ml-auto">
       <ul className="gap-6 hidden md:flex">
         <li>
-          <Link className="font-semibold cursor-pointer" href={'#o-nas'}>
+          <Link
+            className="font-semibold cursor-pointer hover:text-yellow-500 hover:scale-[102%]"
+            href={'#o-nas'}
+          >
             O nás
           </Link>
         </li>
         <li>
-          <Link className="font-semibold cursor-pointer" href={'#poledni-menu'}>
+          <Link
+            className="font-semibold cursor-pointer hover:text-yellow-500 hover:scale-[102%]"
+            href={'#poledni-menu'}
+          >
             Polední menu
           </Link>
         </li>
         <li>
-          <Link className="font-semibold cursor-pointer" href={'/jidelni-listek'}>
+          <Link
+            className="font-semibold cursor-pointer hover:text-yellow-500 hover:scale-[102%]"
+            href={'/jidelni-listek'}
+          >
             Jídelní lístek
           </Link>
         </li>
         <li>
-          <Link className="font-semibold cursor-pointer" href={'#kudy-k-nam'}>
+          <Link
+            className="font-semibold cursor-pointer hover:text-yellow-500 hover:scale-[102%]"
+            href={'#kudy-k-nam'}
+          >
             Kudy k nám?
           </Link>
         </li>
--- app/components/UI/Button.tsx
+++ app/components/UI/Button.tsx
@@ -13,11 +13,11 @@ const Button = ({
   return (
     <button
       className={clsx(
-        'px-6 py-2 md:px-9 md:py-3 text-center w-fit rounded-3xl drop-shadow-lg text-nowrap',
+        'px-6 py-2 md:px-9 md:py-3 text-center w-fit rounded-3xl drop-shadow-lg text-nowrap hover:scale-[102%] hover:drop-shadow-xl',
         className,
         {
           'bg-accent': variant === 'primary',
-          'bg-transparent border-white border-2': variant === 'tertinary',
+          'bg-transparent border-white border-2 hover:border-yellow-500': variant === 'tertinary',
         }
       )}
     >
