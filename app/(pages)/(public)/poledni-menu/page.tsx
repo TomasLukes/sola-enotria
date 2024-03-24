cf05fd53316fefeb4596b56580ccb59dd956585d
TomasLukes <lukest@seznam.cz>
2024-03-24 16:09:45 +0100

    chore: pages structure

--- app/(pages)/(public)/poledni-menu/page.tsx
+++ app/(pages)/(public)/poledni-menu/page.tsx
@@ -1,12 +1,17 @@
+import BackLink from '@/app/components/UI/BackLink';
 import Logo from '@/app/components/UI/Logo';
 
 const PoledniMenu = () => {
   return (
-    <main className="h-screen w-screen gradient-dark text-white grid place-content-center">
-      <Logo className="w-[250px] h-full" />
-      <div className="mt-12">
-        <h1 className="heading text-center">Polední menu</h1>
-        <p className="mt-4">... bude doplněno 🍕</p>
+    <main className="h-screen w-screen gradient-dark text-white">
+      <div className="bg-white bg-grain1 bg-no-repeat bg-cover text-black h-40 md:h-64 pt-16 flex items-center">
+        <h1 className="heading w-full text-center underline decoration-accent">Polední menu</h1>
+      </div>
+      <div className="container-base">
+        <div>
+          <BackLink href="/" className="inset-0 pt-12" />
+          <p className="pt-12">... bude doplněno 🍕</p>
+        </div>
       </div>
     </main>
   );
