e03a2a676eef553a9bf76747b9dafff1d8157ec8
TomasLukes <lukest@seznam.cz>
2024-03-24 16:10:41 +0100

    add: preparation for jidelniListek data

---
+++ app/(pages)/(public)/jidelni-listek/data.ts
@@ -0,0 +1,27 @@
+const bruschettyItems = ['item'];
+const predkrmyItems = ['item'];
+const polevkyItems = ['item'];
+const salatyItems = ['item'];
+const testovinyItems = ['item'];
+const nokyItems = ['item'];
+const pizzaItems = ['item'];
+const masoItems = ['item'];
+const omackyItems = ['item'];
+const detskeMenuItems = ['item'];
+const prilohyItems = ['item'];
+const dezertyItems = ['item'];
+
+export const jidelniListekData = [
+  { heading: 'Bruschetty', items: bruschettyItems },
+  { heading: 'Předkrmy', items: predkrmyItems },
+  { heading: 'Polévky', items: polevkyItems },
+  { heading: 'Saláty', items: salatyItems },
+  { heading: 'Těstoviny', items: testovinyItems },
+  { heading: 'Noky', items: nokyItems },
+  { heading: 'Pizza', items: pizzaItems },
+  { heading: 'Maso', items: masoItems },
+  { heading: 'Omáčky', items: omackyItems },
+  { heading: 'Dětské menu', items: detskeMenuItems },
+  { heading: 'Přílohy', items: prilohyItems },
+  { heading: 'Dezerty', items: dezertyItems },
+];
